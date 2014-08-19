0e48f078447cbd4af0daa7ce99f0f4acf1934205
AlexTseng <guanmingzeng@gmail.com>
2014-08-19 14:24:04 +0800

    add compare function test in selectSort

--- algorithm.js
+++ algorithm.js
@@ -10,7 +10,7 @@
         var i,            // outer loop counter
             j,            // inner loop counter
             n,            // total num of the outer loop
-            min,          // index of min
+            key,          // index of element being put forward
             temp,         // for varible swap
             arr = this;
         if (!compare){
@@ -29,18 +29,18 @@
         // time complexity: O(n^2)
         for (i = 0, n = arr.length - 1; i < n; i++){
 
-            min = i;
+            key = i;
             //  find the smallest element between j to the last
             for (j = i + 1; j <= n; j++){
 
-                if (compare(arr[min],arr[j]) > 0)
-                    min = j;
+                if (compare(arr[key],arr[j]) > 0)
+                    key = j;
             }
 
-            // swap min and i
+            // swap key and i
             temp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = temp;
+            arr[i] = arr[key];
+            arr[key] = temp;
         }
     };
 
